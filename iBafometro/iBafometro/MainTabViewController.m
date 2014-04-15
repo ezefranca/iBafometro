cbc0ed8eb28c8a6919f864514173bf0ca0d3c6ac
Ezequiel França <ezequiel.ifsp@gmail.com>
2014-04-15 14:15:27 -0300

    Melhorias na  interface

--- iBafometro/iBafometro/MainTabViewController.m
+++ iBafometro/iBafometro/MainTabViewController.m
@@ -26,8 +26,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [[self tabBar] setTintColor:[UIColor redColor]];
+    [[self tabBar] setTintColor:[UIColor whiteColor]];
+    [[UITabBar appearance] setSelectedImageTintColor:[UIColor whiteColor]];
     // Do any additional setup after loading the view.
+    
+    // set color of selected icons and text to red
+    self.tabBar.tintColor = [UIColor redColor];
+    [[UITabBarItem appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys: [UIColor whiteColor], NSForegroundColorAttributeName, nil] forState:UIControlStateSelected];
+    
+    
+    // set color of unselected text to green
+    [[UITabBarItem appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor blackColor], NSForegroundColorAttributeName, nil]
+                                             forState:UIControlStateNormal];
+    
+    // set selected and unselected icons
+    UITabBarItem *item0 = [self.tabBar.items objectAtIndex:0];
+    UITabBarItem *item1 = [self.tabBar.items objectAtIndex:1];
+    UITabBarItem *item2 = [self.tabBar.items objectAtIndex:2];
+    UITabBarItem *item3 = [self.tabBar.items objectAtIndex:3];
+    
+    // this way, the icon gets rendered as it is (thus, it needs to be green in this example)
+    item0.image = [[UIImage imageNamed:@"map32.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    item1.image = [[UIImage imageNamed:@"map32.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    item2.image = [[UIImage imageNamed:@"map32.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    item3.image = [[UIImage imageNamed:@"map32.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    
+    // this icon is used for selected tab and it will get tinted as defined in self.tabBar.tintColor
+    item0.selectedImage = [UIImage imageNamed:@"map32"];
+    item1.selectedImage = [UIImage imageNamed:@"map32"];
+    item2.selectedImage = [UIImage imageNamed:@"map32"];
+    item3.selectedImage = [UIImage imageNamed:@"map32"];
 }
 
 - (void)didReceiveMemoryWarning
---
+++ iBafometro/iBafometro/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  test-sk
+//
+
+//  Copyright (c) 2014 EZEQUIEL FRANCA DOS SANTOS. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
